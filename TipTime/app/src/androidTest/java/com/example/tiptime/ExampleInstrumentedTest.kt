8d4fa86e20d26a873575817f3b327ba17306e93c
Damian-Chowanski <dchowanski30@gmail.com>
2022-05-27 20:05:02 +0200

    starting to tests

--- TipTime/app/src/androidTest/java/com/example/tiptime/ExampleInstrumentedTest.kt
+++ TipTime/app/src/androidTest/java/com/example/tiptime/ExampleInstrumentedTest.kt
@@ -1,24 +1,19 @@
 package com.example.tiptime
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class CalculatorTests {
+
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.tiptime", appContext.packageName)
+    fun calculate_20_percent_tip() {
+
     }
 }
