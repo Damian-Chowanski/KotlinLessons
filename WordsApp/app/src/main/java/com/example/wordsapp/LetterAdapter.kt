4dc63caf59ddde309698ea706fb4820e0279f41d
Damian-Chowanski <dchowanski30@gmail.com>
2022-10-02 14:10:50 +0200

    hardcoded letter replaced

--- WordsApp/app/src/main/java/com/example/wordsapp/LetterAdapter.kt
+++ WordsApp/app/src/main/java/com/example/wordsapp/LetterAdapter.kt
@@ -66,7 +66,8 @@ class LetterAdapter :
         holder.button.setOnClickListener{
             val context = holder.view.context
             val intent = Intent(context, DetailActivity::class.java)
-            intent.putExtra("letter", holder.button.text.toString())
+            val letterId = intent?.extras?.getString("letter").toString()
+            intent.putExtra(letterId, holder.button.text.toString())
             context.startActivity(intent)
         }
     }
