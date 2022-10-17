3651e264b05a2ba3f21783fa63c087bf3ad01690
Damian-Chowanski <dchowanski30@gmail.com>
2022-10-17 19:54:00 +0200

    Logs for activity lifecycle added

--- unscrambleApp/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ unscrambleApp/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,8 +1,13 @@
 package com.example.android.unscramble.ui.game
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+    }
 
     private var score = 0
     private var currentWordCount = 0
@@ -14,4 +19,9 @@ class GameViewModel : ViewModel() {
     private var _count = 0
     val count: Int
         get() = _count
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("GameFragment", "GameViewModel destroyed!")
+    }
 }
