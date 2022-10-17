330e15a68c48c55c5a2ead07770e2a94a4f2ef5e
Damian-Chowanski <dchowanski30@gmail.com>
2022-10-17 19:41:13 +0200

    variables moved from fragment to view model

--- unscrambleApp/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ unscrambleApp/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,4 +1,17 @@
 package com.example.android.unscramble.ui.game
 
-class GameViewModel {
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+
+    private var score = 0
+    private var currentWordCount = 0
+    
+    private var _currentScrambledWord = "test"
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
+
+    private var _count = 0
+    val count: Int
+        get() = _count
 }
