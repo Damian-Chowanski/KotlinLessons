86c9fd7e608bc07e4d2de769310eb76caf9de8bd
Damian-Chowanski <dchowanski30@gmail.com>
2022-10-19 18:51:01 +0200

    LiveData created

--- unscrambleApp/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ unscrambleApp/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.android.unscramble.ui.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
@@ -13,8 +15,8 @@ class GameViewModel : ViewModel() {
     val currentWordCount: Int
         get() = _currentWordCount
 
-    private lateinit var _currentScrambledWord: String
-    val currentScrambledWord: String
+    private val _currentScrambledWord = MutableLiveData<String>()
+    val currentScrambleWord: LiveData<String>
         get() = _currentScrambledWord
 
     private var wordsList: MutableList<String> = mutableListOf()
@@ -36,7 +38,7 @@ class GameViewModel : ViewModel() {
         if (wordsList.contains(currentWord)) {
             getNextWord()
         } else {
-            _currentScrambledWord = String(tempWord)
+            _currentScrambledWord.value = String(tempWord)
             ++_currentWordCount
             wordsList.add(currentWord)
         }
