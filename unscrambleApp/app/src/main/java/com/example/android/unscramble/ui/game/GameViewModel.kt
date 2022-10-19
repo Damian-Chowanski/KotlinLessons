35ce961b752263ee1e780fc0fb9b1557a44cfef0
Damian-Chowanski <dchowanski30@gmail.com>
2022-10-19 18:05:27 +0200

    Program verify the words now

--- unscrambleApp/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ unscrambleApp/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -25,34 +25,45 @@ class GameViewModel : ViewModel() {
         getNextWord()
     }
 
-    private fun getNextWord(){
+    private fun getNextWord() {
         currentWord = allWordsList.random()
         val tempWord = currentWord.toCharArray()
         tempWord.shuffle()
 
-        while (String(tempWord).equals(currentWord, false)){
+        while (String(tempWord).equals(currentWord, false)) {
             tempWord.shuffle()
         }
-        if (wordsList.contains(currentWord)){
+        if (wordsList.contains(currentWord)) {
             getNextWord()
-        }else {
+        } else {
             _currentScrambledWord = String(tempWord)
             ++_currentWordCount
             wordsList.add(currentWord)
         }
     }
 
-     fun nextWord(): Boolean{
-        return if (currentWordCount < MAX_NO_OF_WORDS){
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
             getNextWord()
             true
         } else false
+    }
+
+    private fun increaseScore() {
+        _score += SCORE_INCREASE
+    }
+
+    fun isUserWordCorrect(playerWord: String): Boolean {
+        if (playerWord.equals(currentWord, true)) {
+            increaseScore()
+            return true
+        }
+        return false
     }
 
     override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed!")
     }
-
 
 }
